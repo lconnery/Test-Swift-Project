824fb77f5b9b997f94ac3e8852560612f4e3635e
Liam Connery <lconnery61@gmail.com>
2020-07-05 21:15:57 -0400

    Initial Commit

---
+++ Test Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Project
+//
+//  Created by Liam Connery on 7/5/20.
+//  Copyright © 2020 Harrison and Liam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test ProjectTests/Test_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_ProjectTests.swift
+//  Test ProjectTests
+//
+//  Created by Liam Connery on 7/5/20.
+//  Copyright © 2020 Harrison and Liam. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Project
+
+class Test_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
